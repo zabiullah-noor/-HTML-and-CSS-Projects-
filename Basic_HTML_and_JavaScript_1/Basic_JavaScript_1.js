056c7115ecedc8ca87219b2355a40ab60bfaf41f
zabiullah Noor <zabi.asmir541@gmail.com>
2026-06-23 20:54:02 -0400

    adding js folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+//alert("Hello World");
+
+function My_First_Function(){
+		var String ="Kiss me, I am Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+		}
